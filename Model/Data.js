83223af452f724331dfb92fde6ad04eb5612b172
Allison George <aegeorge@udel.edu>
2021-09-11 22:40:19 -0400

    Update Data.js

--- Model/Data.js
+++ Model/Data.js
@@ -34,157 +34,240 @@ export class Data{
     makefruits_linear(){
         var x = 0;
         var y = 0;
+        var set = Math.floor(Math.random() * 4);
+        console.log(set);
         
-        //blueberry
-        var minLength=0;
-        var maxLength=0.45;
-        var minRound=0.45;
-        var maxRound=1.0;
-    
-        for(var i =0; i<40; i++){
-            x=Math.random() * (maxLength - minLength) + minLength;
-            y=Math.random() * (maxRound - minRound) + minRound;
-            this.point("blueberry", [x, y], [0,1]);
-        }
-    
         /*
-        minLength=0.1
-        maxLength=0.4
-        minRound=0.4
-        maxRound=0.6;
-        for(var i =0; i<5; i++){
-            x=Math.random() * (maxLength - minLength) + minLength;
-            y=Math.random() * (maxRound - minRound) + minRound;
-            this.point("blueberry", [x, y], [0,1]);
-        }
+            //blueberry
+            var minLength=0.1;
+            var maxLength=0.35;
+            var minRound=0.5;
+            var maxRound=1.0;
+        
+            for(var i =0; i<20; i++){
+                x=Math.random() * (maxLength - minLength) + minLength;
+                y=Math.random() * (maxRound - minRound) + minRound;
+                this.point("blueberry", [x, y], [0,1]);
+            }
+        
+            
+            minLength=0.25
+            maxLength=0.6
+            minRound=0.75
+            maxRound=1.0
+            for(var i =0; i<20; i++){
+                x=Math.random() * (maxLength - minLength) + minLength;
+                y=Math.random() * (maxRound - minRound) + minRound;
+                this.point("blueberry", [x, y], [0,1]);
+            }
 
-        minLength=0.5;
-        maxLength=1.0;
-        minRound=0.8
-        maxRound=1.0;
-        for(var i =0; i<5; i++){
-            x=Math.random() * (maxLength - minLength) + minLength;
-            y=Math.random() * (maxRound - minRound) + minRound;
-            this.point("blueberry", [x, y], [0,1]);
-        }
+            //strawberry
+            minLength=0.5;
+            maxLength=1.0;
+            minRound=0.0;
+            maxRound=0.25;
+        
+            for(var i =0; i<20; i++){
+                x=Math.random() * (maxLength - minLength) + minLength;
+                y=(Math.random() * (maxRound - minRound) + minRound);
+                this.point("strawberry", [x, y], [1,0]);
+            }
+
+            minLength=0.75;
+            maxLength=1.0;
+            minRound=0.25;
+            maxRound=0.6;
+        
+        
+            for(var i =0; i<20; i++){
+                x=Math.random() * (maxLength - minLength) + minLength;
+                y=(Math.random() * (maxRound - minRound) + minRound);
+                this.point("strawberry", [x, y], [1,0]);
+            }
         */
 
 
-        //strawberry
-        minLength=0.5;
-        maxLength=1.0
-        minRound=0.0
-        maxRound=0.5;
-    
-        for(var i =0; i<40; i++){
-            x=Math.random() * (maxLength - minLength) + minLength;
-            y=(Math.random() * (maxRound - minRound) + minRound);
-            this.point("strawberry", [x, y], [1,0]);
-        }
 
-        minLength=0.8;
-        maxLength=1.0;
-        minRound=0.1
-        maxRound=0.2;
-    
-        /*
-        for(var i =0; i<5; i++){
-            x=Math.random() * (maxLength - minLength) + minLength;
-            y=(Math.random() * (maxRound - minRound) + minRound);
-            this.point("strawberry", [x, y], [1,0]);
-        }
 
-        minLength=1.6;
-        maxLength=2.0;
-        minRound=0.4
-        maxRound=0.7;
-    
-        for(var i =0; i<5; i++){
-            x=Math.random() * (maxLength - minLength) + minLength;
-            y=(Math.random() * (maxRound - minRound) + minRound);
-            this.point("strawberry", [x, y], [1,0]);
-        }
-        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+        
     }
 
-    makefruits_circle(){
+
+
+
+
+
+
+
+
+
+
+
+
+   /* makefruits_circle(){
+
+    //blueberries
+
+        
         //blueberries
         var x = 0;
         var y = 0;
     
-        var minLength=0.9
-        var maxLength=1.2
+        var minLength=0.4;
+        var maxLength=0.6;
         var minRound=0.4;
         var maxRound=0.6;
     
-        for(var i =0; i<80; i++){
-            x=(Math.random() * (maxLength - minLength) + minLength)// + (Math.random() * (0.1 - -0.1) + -0.1)
-            y=(Math.random() * (maxRound - minRound) + minRound)// + (Math.random() * (0.1 - -0.1) + -0.1)
+        var numblue=100;
+        for(var i =0; i<numblue; i++){
+            x=(Math.random() * (maxLength - minLength) + minLength) + (Math.random() * (0.1 - -0.1) + -0.1)
+            y=(Math.random() * (maxRound - minRound) + minRound) + (Math.random() * (0.1 - -0.1) + -0.1)
             this.point("blueberry", [x, y], [0,1]);
         }
 
-        minLength=0
-        maxLength=0
-        minRound=0;;
-        maxRound=0;
-    
-        for(var i =0; i<80; i++){
-            x=(Math.random() * (maxLength - minLength) + minLength)// + (Math.random() * (0.1 - -0.1) + -0.1)
-            y=(Math.random() * (maxRound - minRound) + minRound)// + (Math.random() * (0.1 - -0.1) + -0.1)
-            this.point("blueberry", [x, y], [0,1]);
-        }
-      
+
         //strawberries
+        //sides
         minLength=0;
-        maxLength=0.4;
-        minRound=0;
-        maxRound=1.0;
+        maxLength=0.1;
+        minRound=0.1;
+        maxRound=0.9;
+
+        var numstraw=20;
     
-        for(var i =0; i<20; i++){
+        for(var i =0; i<numstraw; i++){
             x=Math.random() * (maxLength - minLength) + minLength;
             y=(Math.random() * (maxRound - minRound) + minRound);
             this.point("strawberry", [x, y], [1,0]);
         }
 
-        minLength=1.6;
-        maxLength=2.0;
-        minRound=0;
+        minLength=0.9;
+        maxLength=1.0;
+        minRound=0.1;
+        maxRound=0.9;
+    
+        for(var i =0; i<numstraw; i++){
+            x=Math.random() * (maxLength - minLength) + minLength;
+            y=(Math.random() * (maxRound - minRound) + minRound);
+            this.point("strawberry", [x, y], [1,0]);
+        }
+
+        minLength=0.1;
+        maxLength=0.9;
+        minRound=0.9;
         maxRound=1.0;
     
-        for(var i =0; i<20; i++){
+        for(var i =0; i<numstraw; i++){
+            x=Math.random() * (maxLength - minLength) + minLength;
+            y=(Math.random() * (maxRound - minRound) + minRound);
+            this.point("strawberry", [x, y], [1,0]);
+        }
+
+        minLength=0.1;
+        maxLength=0.9;
+        minRound=0;
+        maxRound=0.1;
+    
+        for(var i =0; i<numstraw; i++){
+            x=Math.random() * (maxLength - minLength) + minLength;
+            y=(Math.random() * (maxRound - minRound) + minRound);
+            this.point("strawberry", [x, y], [1,0]);
+        }
+
+        //corners
+        minLength=0;
+        maxLength=0.1;
+        minRound=0.9;
+        maxRound=1.0;
+    
+        for(var i =0; i<numstraw/2; i++){
             x=Math.random() * (maxLength - minLength) + minLength;
             y=(Math.random() * (maxRound - minRound) + minRound);
             this.point("strawberry", [x, y], [1,0]);
         }
 
         minLength=0;
-        maxLength=2.0;
-        minRound=0.85;
-        maxRound=1.0;
+        maxLength=0.1;
+        minRound=0;
+        maxRound=0.1;
     
-        for(var i =0; i<20; i++){
+        for(var i =0; i<numstraw/2; i++){
             x=Math.random() * (maxLength - minLength) + minLength;
             y=(Math.random() * (maxRound - minRound) + minRound);
             this.point("strawberry", [x, y], [1,0]);
         }
 
-        minLength=0;
-        maxLength=2.0;
+        minLength=0.9;
+        maxLength=1.0;
         minRound=0;
-        maxRound=0.15;
+        maxRound=0.1;
     
-        for(var i =0; i<20; i++){
+        for(var i =0; i<numstraw/2; i++){
+            x=Math.random() * (maxLength - minLength) + minLength;
+            y=(Math.random() * (maxRound - minRound) + minRound);
+            this.point("strawberry", [x, y], [1,0]);
+        }
+
+        minLength=0.9;
+        maxLength=1.0;
+        minRound=0.9;
+        maxRound=1.0;
+    
+        for(var i =0; i<numstraw/2; i++){
             x=Math.random() * (maxLength - minLength) + minLength;
             y=(Math.random() * (maxRound - minRound) + minRound);
             this.point("strawberry", [x, y], [1,0]);
         }
     }
+    */
 }
 
 
 export const fruits = new Data(0,["strawberry","blueberry"],["length", "roundness"]);
 fruits.makefruits_linear();
 
-export const fruits_circle = new Data(0, ["strawberry","blueberry"],["length","roundness"]);
-fruits_circle.makefruits_circle();
+//export const fruits_circle = new Data(0, ["strawberry","blueberry"],["length","roundness"]);
+//fruits_circle.makefruits_circle();
     
