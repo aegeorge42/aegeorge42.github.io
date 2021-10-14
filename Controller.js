7579ca427256af3f18fedc8fe190e1a6b492d840
Allison George <aegeorge@udel.edu>
2021-10-14 00:42:10 -0400

    clean up

--- Controller.js
+++ Controller.js
@@ -1,22 +1,2 @@
-//import {Button, textureButton, onButtonDown, getDown} from "./View/Button.js"
-/*import {Button} from "./View/Button.js"
-import {View} from "./View/View.js"
-import {Slide} from "./View/Slide.js"
-import { data } from "./Model/data.js"
-
-
-//import {Neuron, actFns} from "./Model/neuron.js"
-//import {Layer} from "./Model/layer.js"
-
-import {Net} from "./Model/net.js"
-import {actFns} from "./Model/actfns.js"
-*/
-
 import {View} from "./View/View.js"
 export const viewst = new View();
-
-/*<script type="module" src="./Model/data.js"></script>
-
-<script type="module" src="./Model/net.js"></script>
-
-<script type="module" src="./View/Button.js"></script>*/
